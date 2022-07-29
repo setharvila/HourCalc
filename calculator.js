79aee396c1de4dbe0b3c0bee0a57da80b9261750
Seth Arvila <setharvila@hotmail.com>
2022-07-28 19:25:01 -0500

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,73 @@
+window.onload = initAll;
+
+function initAll(){
+    console.log("Made it this far!");
+    
+    
+}
+
+
+function calcHours(){
+
+    var hourIn = document.getElementById('hourIn').value;
+    var minIn = document.getElementById('minIn').value;
+    var amPm;
+    if(document.getElementById('am').checked) {
+        console.log("Time is AM");
+        amPm = "am";
+      }else if(document.getElementById('pm').checked) {
+        console.log("Time is PM");
+        amPm = "pm";
+      }
+
+    var desiredHours = document.getElementById('hoursDesired').value;
+    var hoursAlreadyWorked = document.getElementById('hoursWorked').value;
+
+    var totalNeeded = desiredHours - hoursAlreadyWorked;
+    var returnString = "";
+    //console.log("You need " + totalNeeded + " more hours this week.");
+
+    var hoursNeeded = Math.floor(totalNeeded);
+    var minNeeded = (totalNeeded - hoursNeeded) * 60;
+
+    returnString = ("You need " + Math.trunc(hoursNeeded) + " hours and " + Math.trunc(minNeeded) + " min.<br>");
+    var neededTime =  Math.trunc(hoursNeeded) + " hours and " + Math.trunc(minNeeded) + " min.";
+
+    var outHour = hourIn + hoursNeeded;
+    var outMin = minIn + minNeeded;
+    console.log("outMin = " + outMin);
+
+    if(outMin > 59){
+        var remainMin = (outMin - 60);
+        console.log("made it hereeeeeee");
+        outHour - 1;
+        outMin = remainMin;
+    }
+
+    if(outHour > 12){
+        outHour = outHour % 12;
+        
+        if(amPm == "am") amPm = "pm";
+        else if(amPm == "pm") amPm = "am";
+    }
+    console.log("outMin = " + outMin);
+
+    var txtHour = "" + Math.trunc(outHour);
+    txtHour = "" + ('00'+ outHour).slice(-2);
+    
+    var txtMin = "" + Math.trunc(outMin);
+    txtMin = "" + ('00'+ txtMin).slice(-2);
+
+    console.log("outMin = " + txtMin);
+
+    returnString = returnString + ("You should clock out at " + txtHour + ":" + txtMin + amPm + " for " + desiredHours + " hours.");
+    var clockOutTime = txtHour + ":" + txtMin + amPm;
+
+    console.log(returnString);
+    document.getElementById("result").innerHTML = returnString;
+
+    document.getElementById("clockOutTime").innerHTML = clockOutTime;
+    document.getElementById("needHours").innerHTML = neededTime;
+
+
+}
